9dcd23398e87f6638f5a1cfb13525c879909b747
John Alan McDonald <palisades.lakes@gmail.com>
2017-09-05 15:51:46 -0700

    add linearCombination to Vector interface; minimal implementation

--- src/main/java/palisades/lakes/bench/java/spaces/linear/Vector.java
+++ src/main/java/palisades/lakes/bench/java/spaces/linear/Vector.java
@@ -9,26 +9,29 @@ package palisades.lakes.bench.java.spaces.linear;
  * 
  * @author palisades dot lakes at gmail dot com
  * @since 2017-08-22
- * @version 2017-08-22
+ * @version 2017-09-05
  */
 
 public interface Vector {
 
-  /** Assuming the space has a canonical basis, return the
-   * <code>i</code>-th coordinate in that basis.
-   * <p>
-   * It's questionable whether this really belongs in an interface
-   * implemented by all 'vectors'. A given 'vector' object can
-   * be considered an element of many spaces, and the natural
-   * meaning of 'coordinate' might be different for each.
-   * <p>
-   * Especially problematic that this has to return a 
-   * <code>double</code>, so it's not exactly the value of the
-   * coordinate in most cases.
+  /** Here as a convenience. Doesn't belong in a vector interface,
+   * because same vector can be in many different normed spaces.
    */
-  double coordinate (final int i);
-  
   double l1Norm ();
+  
+  /** Almost certainly would want scalars to be primitive,
+   * not Object references. And implementation might
+   * depend on coordinate type of Vectors, so potentially
+   * 6x6x6x6=1296 methods, more if high precision 
+   * (BigInteger, Rational) reference types are included.
+   * <p>
+   * Here as a convenience. Doesn't belong in a vector interface,
+   * because same vector can be in many different linear spaces,
+   * with differing interpretations of linear combination.
+   */
+  Vector linearCombination (final Number a0,
+                            final Number a1,
+                            final Vector v1);
   
   //--------------------------------------------------------------
 } // end of class
--- src/main/java/palisades/lakes/bench/java/spaces/linear/r2/S2.java
+++ src/main/java/palisades/lakes/bench/java/spaces/linear/r2/S2.java
@@ -10,7 +10,7 @@ import palisades.lakes.bench.java.spaces.linear.Vector;
  * 
  * @author palisades dot lakes at gmail dot com
  * @since 2017-08-22
- * @version 2017-08-22
+ * @version 2017-09-05
  */
 
 public final class S2 implements Vector {
@@ -21,21 +21,104 @@ public final class S2 implements Vector {
   public final short get1 () { return _v1; }
 
   //--------------------------------------------------------------
+  // methods
+  //--------------------------------------------------------------
+  // Likely to be cases where reducing to doubles won't be good
+  // enough. It it was, wouldn't bother with different primitive
+  // coordinate types in the first place.
+  // Possibly more realistic: BigInteger and rational coordinates.
+
+  private final Vector linearCombination (final Number a0,
+                                          final Number a1,
+                                          final B2 v1) {
+    final double a00 = a0.doubleValue();
+    final double a11 = a1.doubleValue();
+    return 
+      D2.make(
+        a00*get0() + a11*v1.get0(), 
+        a00*get1() + a11*v1.get1()); }
+
+  private final Vector linearCombination (final Number a0,
+                                          final Number a1,
+                                          final D2 v1) {
+    final double a00 = a0.doubleValue();
+    final double a11 = a1.doubleValue();
+    return 
+      D2.make(
+        a00*get0() + a11*v1.get0(), 
+        a00*get1() + a11*v1.get1()); }
+
+  private final Vector linearCombination (final Number a0,
+                                          final Number a1,
+                                          final F2 v1) {
+    final double a00 = a0.doubleValue();
+    final double a11 = a1.doubleValue();
+    return 
+      D2.make(
+        a00*get0() + a11*v1.get0(), 
+        a00*get1() + a11*v1.get1()); }
+
+  private final Vector linearCombination (final Number a0,
+                                          final Number a1,
+                                          final I2 v1) {
+    final double a00 = a0.doubleValue();
+    final double a11 = a1.doubleValue();
+    return 
+      D2.make(
+        a00*get0() + a11*v1.get0(), 
+        a00*get1() + a11*v1.get1()); }
+
+  private final Vector linearCombination (final Number a0,
+                                          final Number a1,
+                                          final L2 v1) {
+    final double a00 = a0.doubleValue();
+    final double a11 = a1.doubleValue();
+    return 
+      D2.make(
+        a00*get0() + a11*v1.get0(), 
+        a00*get1() + a11*v1.get1()); }
+
+  private final Vector linearCombination (final Number a0,
+                                          final Number a1,
+                                          final S2 v1) {
+    final double a00 = a0.doubleValue();
+    final double a11 = a1.doubleValue();
+    return 
+      D2.make(
+        a00*get0() + a11*v1.get0(), 
+        a00*get1() + a11*v1.get1()); }
+
+  //--------------------------------------------------------------
   // Vector interface
   //--------------------------------------------------------------
 
   @Override
+  public final Vector linearCombination (final Number a0,
+                                         final Number a1,
+                                         final Vector v1) {
+    if (v1 instanceof B2) {
+      return linearCombination(a0, a1, (B2) v1); }
+    if (v1 instanceof D2) {
+      return linearCombination(a0, a1, (D2) v1); }
+    if (v1 instanceof F2) {
+      return linearCombination(a0, a1, (F2) v1); }
+    if (v1 instanceof I2) {
+      return linearCombination(a0, a1, (I2) v1); }
+    if (v1 instanceof L2) {
+      return linearCombination(a0, a1, (L2) v1); }
+    if (v1 instanceof S2) {
+      return linearCombination(a0, a1, (S2) v1); }
+
+    throw new UnsupportedOperationException(
+      "no method for a linear combination of ("
+        + a0.getClass().getSimpleName() + " * "
+        + getClass().getSimpleName() + ") + ("
+        + a1.getClass().getSimpleName() + " * "
+        + v1.getClass().getSimpleName() + ")");  }
+
+  @Override
   public final double l1Norm () {
     return Math.abs(_v0) + Math.abs(_v1); }
-  
-  @Override
-  public final double coordinate (final int i) {
-    switch (i) {
-    case 0 : return _v0;
-    case 1 : return _v1;
-    default : 
-      throw new IllegalArgumentException(
-        "No " + i + "th coordinate in " + this); } }
 
   //--------------------------------------------------------------
   // construction
