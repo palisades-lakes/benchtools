7013259d781bbabce6292aa3bf8be8d1c6d59b87
John McDonald <palisades.lakes@gmail.com>
2017-09-24 17:30:39 -0700

    codox fixes

--- src/scripts/clojure/palisades/lakes/bench/doc/codox.clj
+++ src/scripts/clojure/palisades/lakes/bench/doc/codox.clj
@@ -8,23 +8,29 @@
   (:require [clojure.java.io :as io]
             [codox.main :as codox]))
 ;;----------------------------------------------------------------
-(let [options  {:name "benchtools"
-                :version "0.0.7" 
-                :description 
-                "Currently unstable utilities for benchmarking clojure and java."
-                :language :clojure
-                :root-path (io/file "./")
-                :output-path "docs/codox"
-                :source-paths ["src/main/clojure"]
-                :source-uri "https://github.com/palisades-lakes/bench/blob/{version}/{filepath}#L{line}"
-                :namespaces :all
-                ;;:doc-paths ["docs"]
-                :doc-files ["README.md"]
-                :html {:namespace-list :flat}
-                ;;:exclude-vars #"^(map)?->\p{Upper}"
-                :metadata {:doc "TODO: write docs"
-                           :doc/format :markdown}
-                :themes [:default]}]
+(let [version "0.0.7"
+      project-name "benchtools"
+      description "Currently unstable utilities for benchmarking clojure and java."
+      options {:name project-name
+               :version version 
+               :description description
+               :language :clojure
+               :root-path (io/file "./")
+               :output-path "docs/codox"
+               :source-paths ["src/main/clojure"]
+               :source-uri (str "https://github.com/palisades-lakes/"
+                                project-name
+                                "/tree/"
+                                project-name
+                                "-{version}/{filepath}#L{line}")
+               :namespaces :all
+               :doc-paths ["docs"]
+               :doc-files ["README.md"]
+               :html {:namespace-list :flat}
+               ;;:exclude-vars #"^(map)?->\p{Upper}"
+               :metadata {:doc "TODO: write docs"
+                          :doc/format :markdown}
+               :themes [:default]}]
   (codox/generate-docs options))
 ;;----------------------------------------------------------------
 
