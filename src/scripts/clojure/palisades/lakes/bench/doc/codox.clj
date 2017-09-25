e96e8e04c688a7da86d6bc58e4211d8fcd505d2f
John McDonald <palisades.lakes@gmail.com>
2017-09-24 17:15:17 -0700

    codox fixes

--- src/scripts/clojure/palisades/lakes/bench/doc/codox.clj
+++ src/scripts/clojure/palisades/lakes/bench/doc/codox.clj
@@ -8,16 +8,15 @@
   (:require [clojure.java.io :as io]
             [codox.main :as codox]))
 ;;----------------------------------------------------------------
-(let [options  {:project 
-                {:name "benchtools"
-                 :version "0.0.7-SNAPSHOT" 
-                 :description 
-                 "Currently unstable utilities for benchmarking clojure and java."}
+(let [options  {:name "benchtools"
+                :version "0.0.7" 
+                :description 
+                "Currently unstable utilities for benchmarking clojure and java."
                 :language :clojure
                 :root-path (io/file "./")
                 :output-path "docs/codox"
                 :source-paths ["src/main/clojure"]
-                ;;:source-uri "https://github.com/palisades-lakes/bench/blob/{version}/{filepath}#L{line}"
+                :source-uri "https://github.com/palisades-lakes/bench/blob/{version}/{filepath}#L{line}"
                 :namespaces :all
                 ;;:doc-paths ["docs"]
                 :doc-files ["README.md"]
